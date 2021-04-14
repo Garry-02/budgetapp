94356755f0867f236b6cbc1e6204969903f51f92
Garry <gurjyotsingh76@gmail.com>
2021-04-14 11:56:50 +0530

    adding new items

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-const createbudget = document.querySelector(".createbtn");
-const additem = document.querySelector(".addbtn");
 const getbudget = document.getElementById("getbudget");
 const itemname = document.getElementById("itemname");
 const itemprice = document.getElementById("itemprice");
 const totalbudget = document.getElementById("totalbudget");
 const remainingbudget = document.getElementById("remaining");
-var itemlist = [];
-
-function createItemList(arr) {
-    return Items = {name : arr[0], price : arr[1]}
-}
+const listitems = document.getElementById("listitems");
+let total = 0;
 
 function create(){
     var budget = getbudget.value ;
     console.log(budget);
-    total = parseFloat(budget)
+    total = parseFloat(budget);
     getbudget.value = "";
     const entry = document.createElement("li");
     entry.appendChild(document.createTextNode(total));
     totalbudget.appendChild(entry);
+}
+
+function additem(){
+    const name = itemname.value ;
+    const price = parseFloat(itemprice.value) ;
+    const itemarray = [];
+    itemarray.push(name);
+    itemarray.push(price);
+    console.log(itemarray);
+    itemname.value = "";
+    itemprice.value = "";
+    const item = document.createElement("li");
+    item.appendChild(document.createTextNode(itemarray));
+    listitems.appendChild(item);
 }
